48a39f3c2d65fbb2836ce1ac88ed944951bf88e2
vishnuragam <vishnu.rky@gmail.com>
2023-10-16 07:28:40 +0200

    Update sever2.js

--- pro2/sever2.js
+++ pro2/sever2.js
@@ -9,124 +9,125 @@ const config = {
   server: "TMEIPRD21",
   database: "XFBLogs",
   options: {
-      trustServerCertificate:true,
-      trustedConnection:false,
-      enableArithAbort:true,
-      useUTC:true
+    trustServerCertificate: true,
+    trustedConnection: false,
+    enableArithAbort: true,
+    useUTC: true
 
   },
-  port:2151,
-  requestTimeout:20000,
+  port: 2151,
+  requestTimeout: 20000,
 }
+
+
+function formatDateTime(dateTime) {
+  const year = dateTime.getFullYear();
+  const month = ('0' + (dateTime.getMonth() + 1)).slice(-2);
+  const day = ('0' + dateTime.getDate()).slice(-2);
+  const hours = ('0' + dateTime.getHours()).slice(-2);
+  const minutes = ('0' + dateTime.getMinutes()).slice(-2);
+  const seconds = ('0' + dateTime.getSeconds()).slice(-2);
+  const formattedDateTime = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+  return formattedDateTime;
+}
+
 app.use(bodyParser.json());
 
 // Serve the HTML file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/TestCount.html');
+  res.sendFile(__dirname + '/TestCount.html');
 });
 
 // Handle data submission
-app.post('/submit', async(req, res) => {
-    
-    const {inputElement,filter,fromDate1,toDate1} = req.body; // Access the data sent from the client
-    console.log('Received data:',inputElement,filter,fromDate1,toDate1 );
-    try {
-        await sql.connect(config,function(err){
-           
-                var reqest = new sql.Request()
-                reqest.query(`select [${inputElement}], Count([${inputElement}]) from [XFBLogs].[dbo].[SBTransfers]
+app.post('/submit', async (req, res) => {
+
+  const { inputElement, filter, fromDate1, toDate1 } = req.body; // Access the data sent from the client
+  console.log('Received data from total count:', inputElement, filter, fromDate1, toDate1);
+  try {
+    await sql.connect(config, function (err) {
+
+      var reqest = new sql.Request()
+      reqest.query(`select [${filter}], Count([${filter}]) from [XFBLogs].[dbo].[SBTransfers]
                    WHERE [timestamp] BETWEEN '${fromDate1}' AND '${toDate1}'
-                   GROUP BY [${inputElement}] ORDER BY COUNT(${inputElement}) desc`,function(err,records){
-                  if(err)console.log(err);
-                  else res.json(records.recordset);
-                  console.log(records.recordset)
-                
-                })
-                })
-    
-    
-        
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
+                   GROUP BY [${filter}] ORDER BY COUNT(${filter}) desc`, function (err, records) {
+        if (err) console.log(err);
+        else res.json(records.recordset);
+        console.log(records.recordset)
 
-    }
-    )
-    app.post('/hour', async(req, res) => {
-    
-      const {inputElement,filter,fromDate1,toDate1} = req.body; // Access the data sent from the client
-      console.log('Received data:',inputElement,filter,fromDate1,toDate1 );
-      try {
-          await sql.connect(config,function(err){
+      })
+    })
 
-            SELECT CAST(timestamp as date) AS ForDate,
-            DATEPART(hour,timestamp) AS OnHour,
-            COUNT(*) AS Totals
-     FROM SBTransfers
-     WHERE sender='FTCOCPS' and [timestamp] >= '2023/02/25 00:00:00' and [timestamp] <= '2023/02/26 04:00:00'
-     GROUP BY CAST(timestamp as date),
-            DATEPART(hour,timestamp) order by ForDate, OnHour
 
-            
-                  var reqest = new sql.Request()
-                  reqest.query(`select [${inputElement}], Count([${inputElement}]) from [XFBLogs].[dbo].[SBTransfers]
-                     WHERE [timestamp] BETWEEN '${fromDate1}' AND '${toDate1}'
-                     GROUP BY [${inputElement}] ORDER BY COUNT(${inputElement}) desc`,function(err,records){
-                    if(err)console.log(err);
-                    else res.json(records.recordset);
-                    console.log(records.recordset)
-                  
-                  })
-                  })
-      
-      
-          
-        } catch (error) {
-          res.status(500).json({ error: error.message });
-        }
-  
-      }
-      )
 
-    app.post('/submit', async(req, res) => {
-    
-      const {inputElement,filter,fromDate1,toDate1} = req.body; // Access the data sent from the client
-      console.log('Received data:',inputElement,filter,fromDate1,toDate1 );
-      try {
-          await sql.connect(config,function(err){
-  
-                  var reqest = new sql.Request()
-                  // reqest.query(`select [${inputElement}], Count(${inputElement}) from [XFBLogs].[dbo].[SBTransfers]
-                  //    WHERE [timestamp] >= '2023/09/26 00:00:00' and [timestamp] <= '2023/09/27 00:00:01'
-                  //    GROUP BY [${inputElement}] ORDER BY COUNT(${inputElement}) desc`,(err,records)){
-                  //   if(err)console.log(err);
-                  //   else res.json(records.recordset);
-                  //   console.log(records.recordset)
-  
-  
-                  
-                  // })
-                  // })
-                  var reqest = new sql.Request()
-                  reqest.query(`select [${inputElement}], Count([${inputElement}]) from [XFBLogs].[dbo].[SBTransfers]
-                     WHERE [timestamp] BETWEEN '${fromDate1}' AND '${toDate1}'
-                     GROUP BY [${inputElement}] ORDER BY COUNT(${inputElement}) desc`,function(err,records){
-                    if(err)console.log(err);
-                    else res.json(records.recordset);
-                    console.log(records.recordset)
-                  
-                  })
-                  })
-      
-      
-          
-        } catch (error) {
-          res.status(500).json({ error: error.message });
-        }
-  
-      }
-      )
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+}
+)
+app.post('/hour', async (req, res) => {
+
+  const { inputElement, filter, fromDate1, toDate1 } = req.body; // Access the data sent from the client
+  console.log('Received data from hour count:', inputElement, filter, fromDate1, toDate1);
+  try {
+    await sql.connect(config, function (err) {
+      var reqest = new sql.Request()
+      reqest.query(`  SELECT CAST(timestamp as date) AS ForDate,
+                   DATEPART(hour,timestamp) AS OnHour,
+                   COUNT(*) AS Totals
+                   FROM SBTransfers
+                   WHERE ${filter}='${inputElement}' and [timestamp] >=  '${fromDate1}' and [timestamp] <= '${toDate1}'
+                   GROUP BY CAST(timestamp as date),
+                   DATEPART(hour,timestamp) order by ForDate, OnHour`, function (err, records) {
+        if (err) console.log(err);
+        else res.json(records.recordset);
+        console.log(records.recordset)
+        console.log(records.recordset[0].ForDate)
+        var dateR = formatDateTime(records.recordset[0].ForDate)
+        console.log(dateR)
+
+
+      })
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+}
+)
+
+app.post('/min', async (req, res) => {
+
+  const { inputElement, filter, fromDate1, toDate1 } = req.body; // Access the data sent from the client
+  console.log('Received data from min count:', inputElement, filter, fromDate1, toDate1);
+  try {
+    await sql.connect(config, function (err) {
+      var reqest = new sql.Request()
+      reqest.query(` 
+          SELECT CAST(timestamp as date) AS ForDate,
+          DATEPART(minute,timestamp) AS EveryMin,
+          COUNT(*) AS Totals
+          FROM SBTransfers
+          WHERE [timestamp] >= '${fromDate1}' and [timestamp] <= '${toDate1}'
+          GROUP BY CAST(timestamp as date),
+          DATEPART(minute,timestamp) order by ForDate, EveryMin
+          `, function (err, records) {
+        if (err) console.log(err);
+        else res.json(records.recordset);
+        console.log(records.recordset)
+
+      })
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+}
+)
+
+
+
 const port = 3001;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
